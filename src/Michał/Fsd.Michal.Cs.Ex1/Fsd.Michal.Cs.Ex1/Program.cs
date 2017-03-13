3d8836b2756062319cbd34c077c43ebf1093715d
kaczmarekmichal <kaczmarek.michal1988@gmail.com>
2017-03-13 22:11:01 +0100

    1st Homework

---
+++ src/Michał/Fsd.Michal.Cs.Ex1/Fsd.Michal.Cs.Ex1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+namespace Fsd.Michal.Cs.Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Headder();
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine($"Argument { i + 1}  of {args.Length}");
+                Console.WriteLine(($"Value : {args[i]}"));
+                Console.WriteLine($"Upper letters: {WordAnalizer.CountUpperLetters(args[i])}");
+                Console.WriteLine($"Lower letters: {WordAnalizer.CountLowerLetter(args[i])}");
+                Console.WriteLine($"Digits: {WordAnalizer.CountDigits(args[i])}\n");
+
+            }
+
+            Footer();
+        }
+
+
+        static void Headder()
+        {
+
+            Console.WriteLine("Exercise 1 - Introduction");
+            Console.WriteLine("------------------------------------------------------------------------");
+            Console.WriteLine($"Today is {DateTime.Now.ToShortDateString()}\n");
+            Console.WriteLine("Console arguments:\n");
+
+
+        }
+
+        static void Footer()
+        {
+            Console.WriteLine("\nClick any key to continue...");
+            Console.ReadKey();
+
+        }
+
+
+    }
+}
---
+++ src/Michał/Fsd.Michal.Cs.Ex1/Fsd.Michal.Cs.Ex1/WordAnalizer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace Fsd.Michal.Cs.Ex1
+{
+    class WordAnalizer
+    {
+
+        public static int CountDigits(string word)
+        {
+            int count = 0;
+            foreach (Char c in word)
+                if (char.IsDigit(c))
+                    count++;
+
+            return count;
+        }
+
+
+        public static int CountLowerLetter(string word)
+        {
+            int count = 0;
+            foreach (Char c in word)
+                if (char.IsLower(c))
+                    count++;
+
+            return count;
+        }
+
+        public static int CountUpperLetters(string word)
+        {
+            int count = 0;
+            foreach (Char c in word)
+                if (char.IsUpper(c))
+                    count++;
+
+            return count;
+        }
+
+
+    }
+}
