e19588253b7d6b17c2aa1d0d6cb1eada1e38bb22
kaczmarekmichal <kaczmarek.michal1988@gmail.com>
2017-03-14 20:37:04 +0100

    fix case no command line arguments

--- src/Michał/Fsd.Michal.Cs.Ex1/Fsd.Michal.Cs.Ex1/Program.cs
+++ src/Michał/Fsd.Michal.Cs.Ex1/Fsd.Michal.Cs.Ex1/Program.cs
@@ -9,20 +9,25 @@ namespace Fsd.Michal.Cs.Ex1
         {
             Headder();
 
-            for (int i = 0; i < args.Length; i++)
+            if (args.Length > 0)
             {
-                Console.WriteLine($"Argument { i + 1}  of {args.Length}");
-                Console.WriteLine(($"Value : {args[i]}"));
-                Console.WriteLine($"Upper letters: {WordAnalyzer.CountUpperLetters(args[i])}");
-                Console.WriteLine($"Lower letters: {WordAnalyzer.CountLowerLetter(args[i])}");
-                Console.WriteLine($"Digits: {WordAnalyzer.CountDigits(args[i])}\n");
+                for (int i = 0; i < args.Length; i++)
+                {
+                    Console.WriteLine($"Argument { i + 1}  of {args.Length}");
+                    Console.WriteLine(($"Value : {args[i]}"));
+                    Console.WriteLine($"Upper letters: {WordAnalyzer.CountUpperLetters(args[i])}");
+                    Console.WriteLine($"Lower letters: {WordAnalyzer.CountLowerLetter(args[i])}");
+                    Console.WriteLine($"Digits: {WordAnalyzer.CountDigits(args[i])}\n");
 
+                }
             }
+
+            else
+                Console.WriteLine("You didn't specify any arguments! Please specify at least one argument.");
 
             Footer();
         }
-
-
+        
         static void Headder()
         {
 
@@ -30,8 +35,7 @@ namespace Fsd.Michal.Cs.Ex1
             Console.WriteLine("------------------------------------------------------------------------");
             Console.WriteLine($"Today is {DateTime.Now.ToShortDateString()}\n");
             Console.WriteLine("Console arguments:\n");
-
-
+            
         }
 
         static void Footer()
@@ -40,7 +44,6 @@ namespace Fsd.Michal.Cs.Ex1
             Console.ReadKey();
 
         }
-
-
+        
     }
 }
