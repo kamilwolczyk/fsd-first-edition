37aa7b7af7e85df0141d7f968c8075660fc8f4b2
vanter2x <vanter2x@gmail.com>
2017-03-15 07:27:03 +0100

    Homework-1

---
+++ src/Slawek/Fsd.Slawek.Cs.Ex1/Fsd.Slawek.Cs.Ex1/ArgsChecker.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+
+
+namespace Fsd.Slawek.Cs.Ex1
+{
+    public static class ArgsChecker
+    {
+        public static int CountUpper(this string str)
+        {
+            return str.Count(char.IsUpper);
+        }
+
+        public static int CountLower(this string str)
+        {
+            return str.Count(char.IsLower);
+        }
+
+        public static int CountDigit(this string str)
+        {
+            return str.Count(char.IsDigit);
+        }
+
+        public static string GetUpper(this string str)
+        {
+            string result = "";
+            str.Where(char.IsUpper).ToList().ForEach(s => result += s);
+            if (string.IsNullOrEmpty(result)) return $"<null>";
+            return result;
+        }
+
+        public static string GetLower(this string str)
+        {
+            string result = "";
+            str.Where(char.IsLower).ToList().ForEach(s => result += s);
+            if (string.IsNullOrEmpty(result)) return $"<null>";
+            return result;
+        }
+
+        public static string GetDigit(this string str)
+        {
+            string result = "";
+            str.Where(char.IsDigit).ToList().ForEach(s => result += s);
+            if (string.IsNullOrEmpty(result)) return $"<null>";
+            return result;
+        }
+    }
+}
