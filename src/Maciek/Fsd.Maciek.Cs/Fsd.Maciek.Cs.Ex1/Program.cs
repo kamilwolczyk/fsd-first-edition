8bebe7f60d4d44496dd9a3bcd4dc58d443a1d83c
Maciek <Maciek@DZIKILAPEK>
2017-03-20 22:01:53 +0100

    Poprawa Code Review

--- src/Maciek/Fsd.Maciek.Cs/Fsd.Maciek.Cs.Ex1/Program.cs
+++ src/Maciek/Fsd.Maciek.Cs/Fsd.Maciek.Cs.Ex1/Program.cs
@@ -4,38 +4,24 @@ namespace Fsd.Maciek.Cs.Ex1
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void RenderHeader(bool isArgumentExists)
         {
-            int argsCount = 0;
-            int lowerCaseCount = 0;
-            int upperCaseCount = 0;
-            int digitsCount = 0;
-            string information = args.Length > 0 ? "Console arguments:\n" : "Oops, there are no arguments.\n";
-
+            string information = isArgumentExists ? "Console arguments:\n" : "Oops, there are no arguments.\n";
             Console.WriteLine($"Excercise 1 - Introduction\n------------------------------------------------\nToday is {DateTime.Now.ToString("yyyy-MM-dd")} \n");
             Console.WriteLine(information);
-                        
-            foreach (var arg in args)
+        }
+
+        static void Main(string[] args)
+        {       
+            RenderHeader(args.Length>0);
+
+            for (int i = 0; i < args.Length; i++)
             {
-                argsCount += 1;
-                Console.WriteLine($"Argument {argsCount} of {args.Length}");
-                Console.WriteLine($"Value: {arg}");
-
-                for (int i = 0; i < arg.Length; i++)
-                {
-                    if (char.IsLower(arg[i]))
-                        lowerCaseCount++;
-                    else
-                        if (char.IsUpper(arg[i]))
-                            upperCaseCount++;
-                        else
-                            if (char.IsDigit(arg[i]))
-                                digitsCount++;
-                }
-
-                Console.WriteLine($"Upper letters: {upperCaseCount}");
-                Console.WriteLine($"Lower letters: {lowerCaseCount}");
-                Console.WriteLine($"Digits: {digitsCount}\n");
+                Console.WriteLine($"Argument {i+1} of {args.Length}");
+                Console.WriteLine($"Value: {args[i]}");
+                Console.WriteLine($"Upper letters: {WordStatistics.CalculateUpperCasesInWord(args[i])}");
+                Console.WriteLine($"Lower letters: {WordStatistics.CalculateLowerCasesInWord(args[i])}");
+                Console.WriteLine($"Digits: {WordStatistics.CalculateDigitsInWord(args[i])}\n");
             }
 
             Console.WriteLine("Click any key to continue...");
