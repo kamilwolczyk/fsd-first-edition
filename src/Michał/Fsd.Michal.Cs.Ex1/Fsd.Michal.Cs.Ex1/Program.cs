bcb5e7166bb14749c1334f7ce1a9993833f30e51
kaczmarekmichal <kaczmarek.michal1988@gmail.com>
2017-03-24 19:11:04 +0100

    code cleaning

--- src/Michał/Fsd.Michal.Cs.Ex1/Fsd.Michal.Cs.Ex1/Program.cs
+++ src/Michał/Fsd.Michal.Cs.Ex1/Fsd.Michal.Cs.Ex1/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 
-
 namespace Fsd.Michal.Cs.Ex1
 {
     class Program
@@ -14,15 +13,16 @@ namespace Fsd.Michal.Cs.Ex1
                 for (int i = 0; i < args.Length; i++)
                 {
                     Console.WriteLine($"Argument { i + 1}  of {args.Length}");
-                    Console.WriteLine(($"Value : {args[i]}"));
+                    Console.WriteLine($"Value : {args[i]}");
                     Console.WriteLine($"Upper letters: {WordAnalyzer.CountUpperLetters(args[i])}");
                     Console.WriteLine($"Lower letters: {WordAnalyzer.CountLowerLetter(args[i])}");
                     Console.WriteLine($"Digits: {WordAnalyzer.CountDigits(args[i])}\n");
                 }
             }
-
             else
+            {
                 Console.WriteLine("You didn't specify any arguments! Please specify at least one argument.");
+            }
 
             Footer();
         }
