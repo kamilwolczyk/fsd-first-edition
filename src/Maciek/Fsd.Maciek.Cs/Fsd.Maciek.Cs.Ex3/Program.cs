589cb656dcbd3b4b163aaaaae9b103beb3d9ce36
Maciek <Maciek@DZIKILAPEK>
2017-04-04 23:19:30 +0200

    Zadanie nr 3 - wersja podstawowa bez obsługi wyjątków

---
+++ src/Maciek/Fsd.Maciek.Cs/Fsd.Maciek.Cs.Ex3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Fsd.Maciek.Cs.Ex3.Models;
+
+namespace Fsd.Maciek.Cs.Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {        
+            Salesman Salesman = new Salesman("Maciek");
+            Warehouse Warehouse = new Warehouse(new List<Product>());
+            Shop Shop = new Shop(Salesman, Warehouse);
+            
+            Shop.HandleCustomer();
+        }
+    }
+}
