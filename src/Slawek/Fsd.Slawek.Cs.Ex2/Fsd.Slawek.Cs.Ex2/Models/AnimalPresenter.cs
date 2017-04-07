e6e00906096d45cec101c90be0cb35ab524c5102
vanter2x <vanter2x@gmail.com>
2017-04-07 12:56:07 +0200

    AnimalPresenter fixes.

--- src/Slawek/Fsd.Slawek.Cs.Ex2/Fsd.Slawek.Cs.Ex2/Models/AnimalPresenter.cs
+++ src/Slawek/Fsd.Slawek.Cs.Ex2/Fsd.Slawek.Cs.Ex2/Models/AnimalPresenter.cs
@@ -5,21 +5,12 @@ namespace Fsd.Slawek.Cs.Ex2.Models
     public class AnimalPresenter
     {
         public string IntroduceAnimal(Animal animal)
-        {
-            return (GetAnimalName(animal) + GetAnimalVoice(animal as IAnimalVoice));
-        }
-
+            => $"{GetAnimalName(animal)} {GetAnimalVoice(animal as IAnimalVoice)}";
+        
         private string GetAnimalVoice(IAnimalVoice voice)
-        {
-            if (voice == null)
-                return "<no voice> \n";
-
-            return $"{voice.Voice}.\n"; 
-        }
-
+            => (voice == null) ? "<no voice> \n" : $"{voice.Voice}.\n"; 
+        
         private string GetAnimalName(Animal animal)
-        {
-            return $"Hello my name is {animal.Name}. ";
-        }
+            => $"Hello my name is {animal.Name}.";
     }
 }
